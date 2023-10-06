8f9a5f82325ca13c61201e9e2727470a45b50e91
AlexandrSabirov <nistkasten@mail.ru>
2023-10-06 19:16:27 +0700

    Реализовал класс Registry

--- seons-crm/vendor/framework/Registry.php
+++ seons-crm/vendor/framework/Registry.php
@@ -2,20 +2,43 @@
 
 namespace framework;
 
+/** 
+ * Класс Registry для добавления и извлечения свойств из реестра
+ */
 class Registry{
     use Traits\TSingleton;
 
+    /**
+     * Свойства приложения
+     * @var array;
+     */
     protected static array $properties = [];
+
+    /**
+     * Метод установки значения в реестре
+     * @param mixed $name
+     * @param mixed $value
+     */
     public static function setProperty($name, $value)
     {
         self::$properties[$name] = $value;
     }
 
+    /**
+     * Метод получения значения из реестре по имени
+     * @param mixed $name
+     * @return mixed
+     */
     public static function getProperty($name)
     {
         return self::$properies[$name] ?? null;
     }   
 
+    /**
+     * Метод получения всех значени из рееста
+     * @param mixed $name
+     * @return array
+     */
     public static function getProperties()
     {
         return self::$properties;
