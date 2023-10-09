943f77770cd2a2d697333c93f4516c94af12312a
AlexandrSabirov <nistkasten@mail.ru>
2023-10-09 18:38:29 +0700

    Добавил комментарии к классу App

--- seons-crm/vendor/framework/App.php
+++ seons-crm/vendor/framework/App.php
@@ -2,19 +2,31 @@
 
 namespace framework;
 
+/**
+ * Класс App инициализирует приложение
+ */
 class App
 {
+    /**
+     * @var object $app;
+     */
     public static $app;
     public function __construct()
     {
+        //Убираем лишние слеши из строки запроса и декодируем 
         $query = trim(urldecode($_SERVER['QUERY_STRING']), '/');
+        //Инициализуем объект для обработки ошибок
         new ErrorHandler();
+        //Инициализируем объект посредством Singleton
         self::$app = Registry::getInstance();
         $this->getParams();
+        //Проверяем есть такой путь
         Route::dispatch($query);
-
     }
 
+    /**
+     * Метод подгрузки данных из файла в реестр
+     */
     private function getParams(){
         $params = require_once CONFIG . "/params.php";
         if (!empty($params)){
