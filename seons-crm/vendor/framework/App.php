bafb4439d0dea4345923c1fedaf26658a69320e6
AlexandrSabirov <nistkasten@mail.ru>
2023-10-09 17:21:51 +0700

    Добавил класс APP

--- seons-crm/vendor/framework/App.php
+++ seons-crm/vendor/framework/App.php
@@ -5,12 +5,23 @@ namespace framework;
 class App
 {
     public static $app;
-    public function __construct(){
+    public function __construct()
+    {
+        $query = trim(urldecode($_SERVER['QUERY_STRING']), '/');
+        new ErrorHandler();
+        self::$app = Registry::getInstance();
+        $this->getParams();
+        Route::dispatch($query);
 
     }
 
     private function getParams(){
-        
+        $params = require_once CONFIG . "/params.php";
+        if (!empty($params)){
+            foreach ($params as $key => $param){
+                self::$app->setProperty($key, $param);
+            }
+        }
     }
 }
 
