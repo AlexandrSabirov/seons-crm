24afbdb09fa08021500d371f8e96f7900436efd7
AlexandrSabirov <nistkasten@mail.ru>
2023-10-10 16:45:43 +0700

    Добавлен класс Model

--- seons-crm/vendor/framework/Model.php
+++ seons-crm/vendor/framework/Model.php
@@ -1,6 +1,40 @@
 <?php
 
 namespace framework;
+use framework\DB;
+abstract class Model {
 
+    /**
+     * Атрибуты
+     * @param array $attributes
+     * 
+     */
+    public array $attributes = [];
+
+    /**
+     * Ошибки
+     * @param array $errors
+     * 
+     */
+    public array $errors = [];
+
+    /**
+     * Правила
+     * @param array $rules
+     * 
+     */
+    public array $rules = [];
+
+    /**
+     * Метки
+     * @param array $labels
+     * 
+     */
+    public array $labels = [];
+    public function __construct()
+    {
+        DB::getInstance();
+    }
+}
 
 ?>
